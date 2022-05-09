69673c62f9a44863b2266e4981516f6a9144b1c7
chirprush <chirpyboat73@gmail.com>
2022-05-08 22:07:30 -0400

    Remove redundant config
    Unfortunately for me, love.filesystem is sandboxed and I don't really
    have a nice way of running a file, so I'm just going to remove it for
    now. Luckily, the existing config file isn't that hard to edit.

--- jishow.love/main.lua
+++ jishow.love/main.lua
@@ -2,14 +2,6 @@ local config = require("default_config")
 local state = require("state")
 
 function love.load(args)
-   local config_file = love.filesystem.getInfo("~/.config/jishow/config.lua")
-   if config_file ~= nil and config_file.type == "file" then
-	  local custom_config = dofile("~/.config/jishow/config.lua")
-
-	  for key, value in pairs(custom_config) do
-		 config[key] = value
-	  end
-   end
    state:load(config)
 end
 
